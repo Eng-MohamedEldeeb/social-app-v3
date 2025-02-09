204c39e488eacb3161386e8a799247361aaab8f8
Mohamed El-Deeb <en.mohamed.eldeeb@gmail.com>
2025-02-09 06:48:35 +0200

    New Group Model
    Created A Group Model

---
+++ src/DB/Models/Group.model.js
@@ -0,0 +1,72 @@
+import mongoose, { Schema, Types, model } from "mongoose";
+// import * as fieldOptions from "../Options/field.length.js";
+
+const groupSchema = new Schema(
+  {
+    cover: {
+      secure_url: String,
+      public_id: String,
+    },
+
+    groupName: {
+      type: String,
+      required: [true, generateMessage("Group Name").errors.required.error]
+
+      minlength: [
+        fieldOptions.fieldLength({
+          fieldName: "Group Name",
+          min: fieldOptions.contentLength.min,
+        }).min.value,
+        fieldOptions.fieldLength({
+          fieldName: "Group Description",
+          max: fieldOptions.contentLength.min,
+        }).min.msg,
+      ],
+      maxlength: [
+        fieldOptions.fieldLength({
+          fieldName: "Group Name",
+          max: fieldOptions.contentLength.max,
+        }).max.value,
+        fieldOptions.fieldLength({
+          fieldName: "Group Description",
+          max: fieldOptions.contentLength.max,
+        }).max.msg,
+      ],
+      },
+
+    info: {
+      type: String,
+      default: "Welcome To Our Group",
+
+      maxlength: [
+        fieldOptions.fieldLength({
+          fieldName: "Group Description",
+          max: fieldOptions.contentLength.max,
+        }).max.value,
+        fieldOptions.fieldLength({
+          fieldName: "Group Description",
+          max: fieldOptions.contentLength.max,
+        }).max.msg,
+      ],
+    },
+
+    private: Boolean,
+
+    members: [{ type: Types.ObjectId, ref: "user" }],
+
+    admins: [{ type: Types.ObjectId, ref: "user" }],
+
+    creator: { type: Types.ObjectId, ref: "user" },
+
+    posts: [{ type: Types.ObjectId, ref: "post" }],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    skipVersioning: true,
+  }
+);
+
+const Group = mongoose.models.Comment || model("group", groupSchema);
+
+export default Group;
